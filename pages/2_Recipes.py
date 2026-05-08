b7310dff8fe382878ee721fe7af206a760af870e
millaas <camilla.piano@yahoo.com>
2026-05-08 15:17:29 +0200

    -

--- pages/2_Recipes.py
+++ pages/2_Recipes.py
@@ -612,7 +612,6 @@ with tab_cuisine:
     else:
         # ── World Map ──────────────────────────────────────────────────────
         st.subheader("🌍 Bites Across Borders")
-        st.caption("A journey through international food traditions")
         st.markdown("- Click on a country on the map to explore its traditional recipes")
         st.markdown("- Not sure where a country is? You can also search it manuelly using the selector below!")
         st.plotly_chart(
